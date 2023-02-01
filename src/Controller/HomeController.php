83eb7e1303c4725374cf2ebf3e2f6841e245778e
yazidham <79407250+yazidham@users.noreply.github.com>
2023-02-01 20:51:33 +0100

    added the logout button

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,17 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    #[IsGranted('ROLE_USER')]
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('ROLE_USER')]
 class ShopController extends AbstractController
 {
     #[Route('/shop', name: 'app_shop')]

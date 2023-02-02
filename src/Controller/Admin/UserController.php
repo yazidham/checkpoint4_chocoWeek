ae6bbf48978424e9733c848c540ea2010bcb9bba
yazidham <79407250+yazidham@users.noreply.github.com>
2023-02-02 14:47:25 +0100

    wip

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,9 +14,12 @@ class UserController extends AbstractController
     #[Route('/show', name : 'show')]
     public function showUsers(UserRepository $userRepository): Response
     {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
+        }
         return $this->render('admin/showUsers.html.twig', [
             'users' => $userRepository->findAll(),
         ]);
     }
+
 }

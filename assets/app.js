956dbf32556e708810216f31bea217dfba06c7ba
yazidham <79407250+yazidham@users.noreply.github.com>
2023-02-04 20:25:29 +0100

    finished the refresh button in ajax

--- assets/app.js
+++ assets/app.js
@@ -11,3 +11,4 @@ import './styles/app.css';
 // start the Stimulus application
 import './bootstrap';
 import './js/draw'
+import './js/refresh'
--- assets/js/draw.js
+++ assets/js/draw.js
@@ -1,22 +1,24 @@
-const drumRoll = document.getElementById('drumRoll');
-const launchButton = document.getElementById('launch-draw')
-const leaveDrawPageButton = document.getElementById('leaveDrawPage')
-const credits = document.getElementById('wrapper')
-const looserName = document.getElementById('looser')
-const body = document.getElementById('body')
-var delayInMilliseconds = 24000; //1 second
+if (document.getElementById('drumRoll')) {
+    const drumRoll = document.getElementById('drumRoll');
+    const launchButton = document.getElementById('launch-draw')
+    const leaveDrawPageButton = document.getElementById('leaveDrawPage')
+    const credits = document.getElementById('wrapper')
+    const looserName = document.getElementById('looser')
+    const body = document.getElementById('body')
+    var delayInMilliseconds = 24000; //1 second
 
-launchButton.addEventListener('click', function () {
-    launchButton.classList.add('hide');
-    body.classList.add('body');
-    credits.classList.toggle('active');
-    drumRoll.play();
-    setTimeout(function() {
-        looserName.classList.add('fixed')
+    launchButton.addEventListener('click', function () {
+        launchButton.classList.add('hide');
+        body.classList.add('body');
         credits.classList.toggle('active');
-        leaveDrawPageButton.classList.remove('hide');
+        drumRoll.play();
         setTimeout(function() {
-            body.classList.remove('body');
+            looserName.classList.add('fixed')
+            credits.classList.toggle('active');
+            leaveDrawPageButton.classList.remove('hide');
+            setTimeout(function() {
+                body.classList.remove('body');
+            }, delayInMilliseconds);
         }, delayInMilliseconds);
-    }, delayInMilliseconds);
-})
+    })
+}
